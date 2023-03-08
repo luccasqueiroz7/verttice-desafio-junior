59bc1fd63b5d88d69af5f79f43c914a408a64fb9
Luccas Queiroz dos Santos <luccasqueiroz7@hotmail.com>
2023-03-08 10:01:35 -0300

    feat/ logica e estilização da calculadora prontas

--- src/pages/ChallengerFive/index.jsx
+++ src/pages/ChallengerFive/index.jsx
@@ -1,20 +1,147 @@
+import { useState } from "react";
 import { Header } from "../../components/Header";
-import { CalculatorSection } from "./style";
+import { CalculatorSection, KeysLi } from "./style";
 
 export const ChallengerFive = () => {
-  const calculator = [7, 8, 9, "÷", 4, 5, 6, "x", 1, 2, 3, "-", 0, ".", "+", "="];
+  const calculator = [
+    "7",
+    "8",
+    "9",
+    "÷",
+    "4",
+    "5",
+    "6",
+    "x",
+    "1",
+    "2",
+    "3",
+    "-",
+    "0",
+    ".",
+    "+",
+    "=",
+  ];
+
+  const [calculatorValue, setCalculatorValue] = useState("");
+  const [firstNumber, setFirstNumber] = useState("");
+  const [operator, setOperator] = useState("");
+
+  function addCalculator(value) {
+    if (value === "Backspace" || value === "Delete") {
+      setCalculatorValue(calculatorValue.slice(0, -1));
+    }
+    if ((value === "." || value === ",") && calculatorValue.indexOf(".") === -1) {
+      setCalculatorValue(calculatorValue + ".");
+    }
+    if (calculator.find((elem) => Number(elem) === Number(value))) {
+      setCalculatorValue(calculatorValue + value);
+    }
+    if (
+      ["+", "-", "*", "x", "/", "÷"].find((elem) => elem === value) &&
+      calculatorValue.length > 0
+    ) {
+      if (firstNumber.length > 0) {
+        switch (operator) {
+          case "+":
+            setFirstNumber(String(Number(firstNumber) + Number(calculatorValue)));
+            setCalculatorValue("");
+            setOperator(value);
+            break;
+
+          case "-":
+            setFirstNumber(String(Number(firstNumber) - Number(calculatorValue)));
+            setCalculatorValue("");
+            setOperator(value);
+            break;
+
+          case "x":
+          case "*":
+            setFirstNumber(String(Number(firstNumber) * Number(calculatorValue)));
+            setCalculatorValue("");
+            setOperator(value);
+            break;
+
+          case "÷":
+          case "/":
+            setFirstNumber(String(Number(firstNumber) / Number(calculatorValue)));
+            setCalculatorValue("");
+            setOperator(value);
+            break;
+
+          default:
+            break;
+        }
+      } else {
+        setFirstNumber(calculatorValue);
+        setCalculatorValue("");
+        setOperator(value);
+      }
+    }
+
+    if ((value === "=" || value === "Enter") && calculatorValue.length > 0) {
+      switch (operator) {
+        case "+":
+          setCalculatorValue(String(Number(firstNumber) + Number(calculatorValue)));
+          setFirstNumber("");
+          setOperator("");
+          break;
+
+        case "-":
+          setCalculatorValue(String(Number(firstNumber) - Number(calculatorValue)));
+          setFirstNumber("");
+          setOperator("");
+          break;
+
+        case "x":
+        case "*":
+          setCalculatorValue(String(Number(firstNumber) * Number(calculatorValue)));
+          setFirstNumber("");
+          setOperator("");
+          break;
+
+        case "÷":
+        case "/":
+          setCalculatorValue(String(Number(firstNumber) / Number(calculatorValue)));
+          setFirstNumber("");
+          setOperator("");
+          break;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  function clearCalculator() {
+    setCalculatorValue("");
+    setFirstNumber("");
+    setOperator("");
+  }
+
   return (
     <>
       <Header></Header>
       <CalculatorSection>
-        <div>
-          <input type="text" />
-          {/* Utilizar o resultado do input par começar proxima operação*/}
+        <div className="calculator">
+          <div>
+            <button className="clearCalculator" onClick={clearCalculator}>
+              Limpar
+            </button>
+            <div className="inputFirstNumber">
+              {firstNumber} {operator}
+            </div>
+            <input
+              type="text"
+              value={calculatorValue}
+              onKeyDownCapture={(e) => addCalculator(e.key)}
+              readOnly
+            />
+          </div>
           <ul>
             {calculator.map((elem, index) => (
-              <li key={index}>
-                <button>{elem}</button>
-              </li>
+              <KeysLi keyColor={isNaN(elem)} key={index}>
+                <button onClick={() => addCalculator(elem)}>{elem}</button>
+              </KeysLi>
             ))}
           </ul>
         </div>
--- src/pages/ChallengerFive/style.js
+++ src/pages/ChallengerFive/style.js
@@ -4,22 +4,43 @@ export const CalculatorSection = styled.section`
   display: flex;
   justify-content: center;
 
-  // Adicionar box-shadow
-  // Adicionar classname
   > div {
     display: flex;
     flex-direction: column;
     width: 70%;
     max-width: 200px;
-    padding: 30px 10px;
+    padding: 20px 10px 30px;
     background-color: var(--grey-3);
     border-radius: 10px;
     gap: 15px;
 
-    > input {
-      height: 30px;
-      padding: 10px 5px;
-      border-radius: 4px;
+    div {
+      display: flex;
+      flex-direction: column;
+      gap: 0px;
+      > .clearCalculator {
+        display: flex;
+        align-self: flex-end;
+        background-color: var(--black);
+        color: var(--white);
+        padding: 5px;
+        border-radius: 5px;
+        margin-bottom: 10px;
+        font-weight: 600;
+      }
+      > input,
+      .inputFirstNumber {
+        height: 30px;
+        border-radius: 0 0 4px 4px;
+        border: none;
+        text-align: end;
+        padding: 10px 5px;
+      }
+
+      > .inputFirstNumber {
+        background-color: var(--white);
+        border-radius: 4px 4px 0 0;
+      }
     }
 
     > ul {
@@ -27,7 +48,6 @@ export const CalculatorSection = styled.section`
       justify-content: space-between;
       flex-wrap: wrap;
       gap: 5px;
-
       > li {
         display: flex;
         justify-content: center;
@@ -37,11 +57,16 @@ export const CalculatorSection = styled.section`
         > button {
           width: 100%;
           padding: 4px 2px;
-          background-color: var(--black);
           color: var(--white);
           font-size: 20px;
         }
       }
     }
+  }
+`;
+
+export const KeysLi = styled.li`
+  > button {
+    background-color: ${(props) => (props.keyColor ? "var(--black)" : "var(--purple)")};
   }
 `;
